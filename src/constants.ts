546b88587d8e2a260785b240a1b892500b2437c0
Cassiano D'Andrea <cassiano.dandrea@gmail.com>
2026-05-13 00:02:50 -0300

    Unificando as classes do demo do cubo mágico num único arquivo

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,4 @@
-import { Coord3D } from './rubik_cube'
-
-export const GRAVITY = 0.05
+export const GRAVITY = 0.25
 export const FPS = 120
 export const DEPTH = 2000
 export const SCREEN_Z_DISTANCE = 400
@@ -9,32 +7,3 @@ export const CIRCLE_SEGMENTS = 36
 export const SPHERE_LONGITUDE_LINES = 16
 export const SPHERE_LATITUDE_LINES = 16
 export const LINE_SEGMENTS = 20
-
-export const CUBIE_SIZE = 40
-export const CUBIE_SPACING = 30
-export const CUBIES_PER_AXIS = 5
-
-export const FACES: Record<string, number> = {
-  front: 0,
-  right: 1,
-  back: 2,
-  left: 3,
-  top: 4,
-  bottom: 5,
-} as const
-export const FACE_COLORS: Record<number, string> = {
-  [FACES.front]: 'green',
-  [FACES.right]: 'red',
-  [FACES.back]: 'blue',
-  [FACES.left]: 'orange',
-  [FACES.top]: 'white',
-  [FACES.bottom]: 'yellow',
-} as const
-export const FACE_NORMALS: Record<number, Coord3D> = {
-  [FACES.front]: [0, 0, 1], // Normal unit vector towards +z
-  [FACES.right]: [1, 0, 0], // Normal unit vector towards +x
-  [FACES.back]: [0, 0, -1], // Normal unit vector towards -z
-  [FACES.left]: [-1, 0, 0], // Normal unit vector towards -x
-  [FACES.top]: [0, 1, 0], // Normal unit vector towards +y
-  [FACES.bottom]: [0, -1, 0], // Normal unit vector towards -y
-} as const
--- src/main2.ts
+++ src/main2.ts
@@ -1,18 +1,175 @@
-import { CUBIE_SIZE, CUBIES_PER_AXIS, FPS } from './constants'
-import { createFrameLoop, millis, togglePause } from './utils'
+import { FPS, SCREEN_Z_DISTANCE } from './constants'
+import {
+  createFrameLoop,
+  fps,
+  millis,
+  timesForEach,
+  togglePause,
+} from './utils'
 import {
   animation,
   background,
+  isolateTransformations,
+  planeXY,
+  planeXZ,
+  planeYZ,
   processDeferredRenders,
   render3dAxes,
   resetTransformationMatrix,
   rotateX,
   rotateY,
   text2d,
+  transform,
+  translate,
 } from './primitives'
-import { $v } from './vector'
-import { RubikCube } from './rubik_cube'
-import { PI, sin } from './math_utils'
+import { $v, Vector } from './vector'
+import { abs, PI, sin } from './math_utils'
+import { Tuple } from './utility_types'
+
+const CUBIE_SIZE = 40
+const CUBIE_SPACING = 30
+const CUBIES_PER_AXIS = 5
+
+const FACES: Record<string, number> = {
+  front: 0,
+  right: 1,
+  back: 2,
+  left: 3,
+  top: 4,
+  bottom: 5,
+} as const
+const FACE_COLORS: Record<number, string> = {
+  [FACES.front]: 'green',
+  [FACES.right]: 'red',
+  [FACES.back]: 'blue',
+  [FACES.left]: 'orange',
+  [FACES.top]: 'white',
+  [FACES.bottom]: 'yellow',
+} as const
+const FACE_NORMALS: Record<number, Coord3D> = {
+  [FACES.front]: [0, 0, 1], // Normal unit vector towards +z
+  [FACES.right]: [1, 0, 0], // Normal unit vector towards +x
+  [FACES.back]: [0, 0, -1], // Normal unit vector towards -z
+  [FACES.left]: [-1, 0, 0], // Normal unit vector towards -x
+  [FACES.top]: [0, 1, 0], // Normal unit vector towards +y
+  [FACES.bottom]: [0, -1, 0], // Normal unit vector towards -y
+} as const
+
+export type Coord3D = Tuple<number, 3>
+
+export class RubikCube {
+  cubies: Cubie[]
+
+  constructor(
+    public cubieSize: number,
+    public cubiesPerAxis: number,
+  ) {
+    this.cubies = []
+
+    for (let x = -(cubiesPerAxis - 1) / 2; x <= (cubiesPerAxis - 1) / 2; x++)
+      for (let y = -(cubiesPerAxis - 1) / 2; y <= (cubiesPerAxis - 1) / 2; y++)
+        for (
+          let z = -(cubiesPerAxis - 1) / 2;
+          z <= (cubiesPerAxis - 1) / 2;
+          z++
+        ) {
+          const cubie = new Cubie(this, x, y, z)
+
+          this.cubies.push(cubie)
+        }
+  }
+
+  render() {
+    this.cubies.forEach(cubie => cubie.render())
+  }
+}
+
+export class Cubie {
+  position: Vector
+  faces: CubieFace[]
+
+  constructor(
+    public cube: RubikCube,
+    x: number,
+    y: number,
+    z: number,
+  ) {
+    this.position = $v(x, y, z)
+    this.faces = []
+
+    timesForEach(6, i => {
+      const face = new CubieFace(this, FACE_COLORS[i], FACE_NORMALS[i])
+
+      this.faces.push(face)
+    })
+  }
+
+  get size() {
+    return this.cube.cubieSize
+  }
+
+  get center() {
+    return this.position.mult(this.size + CUBIE_SPACING, false)
+  }
+
+  get distanceFromCamera() {
+    return transform(this.center).z + SCREEN_Z_DISTANCE
+  }
+
+  render() {
+    this.faces.forEach(face => face.render())
+  }
+}
+
+export class CubieFace {
+  normal: Vector
+
+  constructor(
+    public cubie: Cubie,
+    public color: string,
+    normal: Coord3D,
+  ) {
+    this.normal = $v(...normal)
+  }
+
+  get size() {
+    return this.cubie.size
+  }
+
+  get center() {
+    return this.cubie.center.add(this.normal.mult(this.size / 2, false), false)
+  }
+
+  get distanceFromCamera() {
+    return transform(this.center).z + SCREEN_Z_DISTANCE
+  }
+
+  render() {
+    isolateTransformations(() => {
+      // Move to the face's center.
+      translate(this.center)
+
+      if (abs(this.normal.x) === 1)
+        planeYZ(this.size, this.size, {
+          color: this.color,
+          lineWidth: 2,
+          opacity: 1,
+        })
+      else if (abs(this.normal.y) === 1)
+        planeXZ(this.size, this.size, {
+          color: this.color,
+          lineWidth: 2,
+          opacity: 1,
+        })
+      else if (abs(this.normal.z) === 1)
+        planeXY(this.size, this.size, {
+          color: this.color,
+          lineWidth: 2,
+          opacity: 1,
+        })
+    })
+  }
+}
 
 animation.onclick = () => togglePause()
 
@@ -20,6 +177,7 @@ const cube = new RubikCube(CUBIE_SIZE, CUBIES_PER_AXIS)
 
 const draw = () => {
   // console.log({ fps: fps(), millis: millis(), frameCount: frameCount() })
+  console.log({ fps: fps() })
 
   background('lightGray')
 
