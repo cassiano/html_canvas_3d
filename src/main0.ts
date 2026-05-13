b26a25bccca60275f8acd415fa921712acaefaee
Cassiano D'Andrea <cassiano.dandrea@gmail.com>
2026-05-13 00:40:19 -0300

    Unificando as classes do demo de "movers" num único arquivo

--- src/main0.ts
+++ src/main0.ts
@@ -3,6 +3,7 @@ import { createFrameLoop, millis, togglePause } from './utils'
 import {
   animation,
   background,
+  cube,
   isolateTransformations,
   planeXZ,
   processDeferredRenders,
@@ -10,12 +11,168 @@ import {
   resetTransformationMatrix,
   rotateX,
   rotateY,
+  rotateZ,
+  sphere,
   text2d,
   translate,
 } from './primitives'
-import { $v } from './vector'
-import { CubeSphereMover } from './cube_sphere_mover'
+import { $v, Vector } from './vector'
 import { PI } from './math_utils'
+
+class Mover3D {
+  position: Vector
+  velocity: Vector
+  acceleration: Vector
+
+  constructor(
+    public mass: number,
+    x: number,
+    y: number,
+    z: number,
+  ) {
+    this.position = $v(x, y, z)
+    this.velocity = $v(0, 0, 0)
+    this.acceleration = $v(0, 0, 0)
+  }
+
+  update(depth: number) {
+    this.position.add(this.velocity)
+
+    if (!this.touchedBottom(depth)) this.velocity.add(this.acceleration)
+
+    this.acceleration.mult(0)
+  }
+
+  // F = m x a  => a = F / m
+  applyForce(force: Vector) {
+    this.addAcceleration(force.div(this.mass, false))
+  }
+
+  addAcceleration(acceleration: Vector) {
+    this.acceleration.add(acceleration)
+  }
+
+  render({
+    xAngle,
+    yAngle,
+    zAngle,
+  }: { xAngle?: number; yAngle?: number; zAngle?: number } = {}) {
+    throw new Error('Not implemented')
+  }
+
+  distanceFromCenterToBorder(): number {
+    throw new Error('Not implemented')
+  }
+
+  touchedBottom(depth: number) {
+    return -this.position.y + this.distanceFromCenterToBorder() > depth
+  }
+
+  checkEdges(depth: number) {
+    if (this.touchedBottom(depth)) this.velocity.y *= -1
+  }
+}
+
+class CubeMover extends Mover3D {
+  constructor(
+    mass: number,
+    x: number,
+    y: number,
+    z: number,
+    public size: number,
+  ) {
+    super(mass, x, y, z)
+  }
+
+  distanceFromCenterToBorder() {
+    return this.size / 2
+  }
+
+  render({
+    xAngle,
+    yAngle,
+    zAngle,
+  }: { xAngle?: number; yAngle?: number; zAngle?: number } = {}) {
+    isolateTransformations(() => {
+      translate(this.position)
+
+      if (xAngle !== undefined) rotateX(xAngle)
+      if (yAngle !== undefined) rotateY(yAngle)
+      if (zAngle !== undefined) rotateZ(zAngle)
+
+      cube(this.size, { color: 'orange' })
+    })
+  }
+}
+
+class SphereMover extends Mover3D {
+  constructor(
+    mass: number,
+    x: number,
+    y: number,
+    z: number,
+    public radius: number,
+  ) {
+    super(mass, x, y, z)
+  }
+
+  distanceFromCenterToBorder() {
+    return this.radius
+  }
+
+  render({
+    xAngle,
+    yAngle,
+    zAngle,
+  }: { xAngle?: number; yAngle?: number; zAngle?: number } = {}) {
+    isolateTransformations(() => {
+      translate(this.position)
+
+      if (xAngle !== undefined) rotateX(xAngle)
+      if (yAngle !== undefined) rotateY(yAngle)
+      if (zAngle !== undefined) rotateZ(zAngle)
+
+      sphere(this.radius, { color: 'rgb(161, 12, 12)', lineWidth: 1 })
+    })
+  }
+}
+
+class CubeSphereMover extends Mover3D {
+  constructor(
+    mass: number,
+    x: number,
+    y: number,
+    z: number,
+    public radius: number,
+  ) {
+    super(mass, x, y, z)
+  }
+
+  distanceFromCenterToBorder() {
+    return this.radius
+  }
+
+  render({
+    xAngle,
+    yAngle,
+    zAngle,
+  }: { xAngle?: number; yAngle?: number; zAngle?: number } = {}) {
+    isolateTransformations(() => {
+      translate(this.position)
+
+      if (xAngle !== undefined) rotateX(xAngle)
+      if (yAngle !== undefined) rotateY(yAngle)
+      if (zAngle !== undefined) rotateZ(zAngle)
+
+      sphere(this.radius, { color: 'rgb(161, 12, 12)', lineWidth: 1 })
+      cube(this.radius * 2 * 0.7, {
+        color: 'black',
+        lineWidth: 2,
+        opacity: 0.15,
+      })
+    })
+  }
+}
 
 animation.onclick = () => togglePause()
 
