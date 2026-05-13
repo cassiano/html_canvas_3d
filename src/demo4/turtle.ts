f90785b70c9bef810f43d803bba119609b213378
Cassiano D'Andrea <cassiano.dandrea@gmail.com>
2026-05-13 18:24:33 -0300

    Refatorando turtle

--- src/demo4/turtle.ts
+++ src/demo4/turtle.ts
@@ -1,5 +1,5 @@
 import {
-  box,
+  cube,
   line,
   point,
   pop,
@@ -13,29 +13,6 @@ import { $v } from '../vector'
 
 const TINY_AXIS_LENGTH = 50
 
-const renderTiny3dAxes = () => {
-  const xNeg = $v(-TINY_AXIS_LENGTH / 2, 0, 0)
-  const xPos = $v(TINY_AXIS_LENGTH / 2, 0, 0)
-
-  const yNeg = $v(0, -TINY_AXIS_LENGTH / 2, 0)
-  const yPos = $v(0, TINY_AXIS_LENGTH / 2, 0)
-
-  const zNeg = $v(0, 0, -TINY_AXIS_LENGTH / 2)
-  const zPos = $v(0, 0, TINY_AXIS_LENGTH / 2)
-
-  // X-axis
-  line(xNeg, xPos, { color: 'darkRed' })
-  point(xPos, { color: 'red', size: 5 })
-
-  // Y-axis
-  line(yNeg, yPos, { color: 'darkGreen' })
-  point(yPos, { color: 'green', size: 5 })
-
-  // Z-axis
-  line(zNeg, zPos, { color: 'darkBlue' })
-  point(zPos, { color: 'blue', size: 5 })
-}
-
 export class Turtle {
   squareCount: number = 0
 
@@ -43,22 +20,13 @@ export class Turtle {
     public length: number,
     public angle: number,
     public cubeScale: number,
-  ) {
-    this.resetSquareCount()
-  }
-
-  resetSquareCount() {
-    this.squareCount = 0
-  }
+  ) {}
 
   render(sentence: string) {
-    this.resetSquareCount()
-
     for (const character of sentence) {
       switch (character) {
         case '◼':
-          box(this.length, this.length, this.length, { color: 'lightGray' })
-          this.squareCount++
+          cube(this.length, { color: '#AAA' })
           translate(this.length, 0, 0)
           break
         case '◻':
@@ -101,25 +69,48 @@ export class Turtle {
           pop()
           break
         case '½':
-          scale(1 / 2, 1 / 2, 1 / 2)
+          scale(1 / 2)
           break
         case '②':
         case '⑵':
         case '2':
-          scale(2, 2, 2)
+          scale(2)
           break
         case '▽':
-          scale(this.cubeScale, this.cubeScale, this.cubeScale)
+          scale(this.cubeScale)
           break
         case '△':
-          scale(1 / this.cubeScale, 1 / this.cubeScale, 1 / this.cubeScale)
+          scale(1 / this.cubeScale)
           break
         case '👀':
-          renderTiny3dAxes()
+          this.renderTiny3dAxes()
           break
         default:
           throw new Error(`Unrecognized character ${character}`)
       }
     }
+  }
+
+  private renderTiny3dAxes() {
+    const xNeg = $v(-TINY_AXIS_LENGTH / 2, 0, 0)
+    const xPos = $v(TINY_AXIS_LENGTH / 2, 0, 0)
+
+    const yNeg = $v(0, -TINY_AXIS_LENGTH / 2, 0)
+    const yPos = $v(0, TINY_AXIS_LENGTH / 2, 0)
+
+    const zNeg = $v(0, 0, -TINY_AXIS_LENGTH / 2)
+    const zPos = $v(0, 0, TINY_AXIS_LENGTH / 2)
+
+    // X-axis
+    line(xNeg, xPos, { color: 'darkRed' })
+    point(xPos, { color: 'red', size: 5 })
+
+    // Y-axis
+    line(yNeg, yPos, { color: 'darkGreen' })
+    point(yPos, { color: 'green', size: 5 })
+
+    // Z-axis
+    line(zNeg, zPos, { color: 'darkBlue' })
+    point(zPos, { color: 'blue', size: 5 })
   }
 }
