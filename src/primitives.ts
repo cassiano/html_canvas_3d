cb46b4143c17d663082454208878fb41f0967c05
Cassiano D'Andrea <cassiano.dandrea@gmail.com>
2026-05-23 01:43:13 -0300

    Refatorando `cone()`

--- src/primitives.ts
+++ src/primitives.ts
@@ -676,7 +676,7 @@ export const circle2d = (radius: number, options: ShapeOptions = {}) => {
     const p1 = $v(radius * sin(theta1), radius * cos(theta1))
     const p2 = $v(radius * sin(theta2), radius * cos(theta2))
 
-    // Connects the center to two points on the perimeter to form a slice
+    // Form a slice by connecting the two points on the perimeter to the circle center (origin).
     triangle2d(ORIGIN, p1, p2, options)
   }
 }
@@ -689,8 +689,6 @@ export const cone = (
   const step = (2 * PI) / CIRCLE_SEGMENTS
   const tip = $v(0, 0, height)
 
-  circle2d(radius, options)
-
   for (let i = 0; i < CIRCLE_SEGMENTS; i++) {
     const theta1 = i * step
     const theta2 = (i + 1) * step
@@ -698,8 +696,10 @@ export const cone = (
     const p1 = $v(radius * sin(theta1), radius * cos(theta1), 0)
     const p2 = $v(radius * sin(theta2), radius * cos(theta2), 0)
 
-    // Connects the cone tip to two points on the perimeter to form a slice.
+    // Form 2 slices, one connecting the above two points on the perimeter to the cone tip
+    // and another to the cone center (origin).
     triangle2d(tip, p1, p2, options)
+    triangle2d(ORIGIN, p1, p2, options)
   }
 }
 
