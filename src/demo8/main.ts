1fa040c03627a0a768825bae14ded1743c65fe50
Cassiano D'Andrea <cassiano.dandrea@gmail.com>
2026-05-23 12:16:27 -0300

    Novo demo (8)

---
+++ src/demo8/main.ts
@@ -0,0 +1,54 @@
+//////////////////////////
+// Rotating Sphere Demo //
+//////////////////////////
+
+import { FPS } from '../constants.ts'
+import { createFrameLoop, fps, togglePause, millis } from '../utils.ts'
+import {
+  animation,
+  background,
+  render3dScene,
+  render3dAxes,
+  resetTransformationMatrix,
+  text2d,
+} from '../primitives.ts'
+import { $v } from '../vector_3d.ts'
+import { PI } from '../math_utils.ts'
+import { rotateX, rotateZ, cylinder } from '../primitives.ts'
+
+// -------------------------------------------------------------------------------------------------
+
+animation.onclick = () => togglePause()
+
+const draw = () => {
+  // console.log({ fps: fps(), millis: millis(), frameCount: frameCount() })
+  console.log({ fps: fps() })
+
+  background('lightGray')
+
+  rotateX(-PI / 3)
+  rotateZ(-millis() / 2000)
+
+  render3dAxes()
+
+  cylinder(150, 230, {
+    color: 'gray',
+    opacity: 0.5,
+  })
+}
+
+const onPaused = () => {
+  text2d('PAUSED', $v(0, 300))
+}
+
+const { start, stop } = createFrameLoop(
+  () => {
+    resetTransformationMatrix()
+    draw()
+    render3dScene()
+  },
+  onPaused,
+  FPS,
+)
+
+export { start, stop }
