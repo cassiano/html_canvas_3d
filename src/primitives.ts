35d660ad77ddb62cc65280ae23717d18915e51bb
Cassiano D'Andrea <cassiano.dandrea@gmail.com>
2026-05-23 20:18:40 -0300

    Refatorando `sphere()`

--- src/primitives.ts
+++ src/primitives.ts
@@ -660,24 +660,27 @@ export const sphere = (radius: number, options: ShapeOptions = {}) => {
   const latSegments = SPHERE_LATITUDE_LINES + 1
 
   const getPoint = (latIndex: number, longIndex: number): Vector3d => {
-    const phi = (latIndex / latSegments) * PI // Latitude (0 to PI)
-    const theta = (longIndex / longSegments) * 2 * PI // Longitude (0 to 2.PI)
+    const latAngle = (latIndex / latSegments) * PI // 0 ≤ latitude ≤ PI
+    const longAngle = (longIndex / longSegments) * 2 * PI // 0 ≤ longitude ≤ 2.PI
 
     // RHR Mapping:
-    return $v(sin(phi) * cos(theta), cos(phi), sin(phi) * sin(theta)).mult(
-      radius,
-    )
+    return $v(
+      sin(latAngle) * cos(longAngle),
+      cos(latAngle),
+      sin(latAngle) * sin(longAngle),
+    ).mult(radius)
   }
 
-  for (let lat = 0; lat < latSegments; lat++)
-    for (let long = 0; long < longSegments; long++) {
-      const p1 = getPoint(lat, long)
-      const p2 = getPoint(lat, long + 1)
-      const p3 = getPoint(lat + 1, long + 1)
-      const p4 = getPoint(lat + 1, long)
+  timesForEach(latSegments, latIndex => {
+    timesForEach(longSegments, longIndex => {
+      const p1 = getPoint(latIndex, longIndex)
+      const p2 = getPoint(latIndex, longIndex + 1)
+      const p3 = getPoint(latIndex + 1, longIndex + 1)
+      const p4 = getPoint(latIndex + 1, longIndex)
 
       quadrilateral2d(p1, p2, p3, p4, options)
-    }
+    })
+  })
 }
 
 type CircularShapeOptions = ShapeOptions & { circleSegments?: number }
