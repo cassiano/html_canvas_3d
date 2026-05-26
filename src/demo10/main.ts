899b01dbc76ab4393b7f3040fbbeea10d0895a2f
Cassiano D'Andrea <cassiano.dandrea@gmail.com>
2026-05-26 14:43:28 -0300

    Refatorando demo 10

--- src/demo10/main.ts
+++ src/demo10/main.ts
@@ -21,11 +21,9 @@ import { rotateZ, quadrilateral2d } from '../primitives.ts'
 
 // -------------------------------------------------------------------------------------------------
 
-const NOISE_DIMENSIONS = [50, 50] as [number, number]
+const NOISE_DIMENSIONS = [64, 64] as [number, number]
 
-const GRID_WIDTH = 250
-const GRID_HEIGHT = 250
-const GRID_DEPTH = 200
+const GRID = { width: 500, height: 500, depth: 500 }
 
 const noise = new PerlinNoise({
   // seed: 123,
@@ -33,10 +31,10 @@ const noise = new PerlinNoise({
 })
 
 const randomCoords = timesMapN(NOISE_DIMENSIONS, (i, j) => {
-  const x = i / NOISE_DIMENSIONS[0] // 0 ≤ x ≤ 1
-  const y = j / NOISE_DIMENSIONS[1] // 0 ≤ y ≤ 1
+  const x = i / NOISE_DIMENSIONS[0] // 0 < x < 1
+  const y = j / NOISE_DIMENSIONS[1] // 0 < y < 1
 
-  const k = noise.noise([x, y]) // -1 ≤ x ≤ 1
+  const k = noise.noise([x, y]) // -1 < x < 1
 
   return { x, y, z: k }
 })
@@ -60,24 +58,24 @@ const draw = () => {
       const { x: xD, y: yD, z: zD } = randomCoords[i][j + 1]
 
       const pointA = $v(
-        project(xA, 0, 1, -GRID_WIDTH, GRID_WIDTH),
-        project(yA, 0, 1, -GRID_HEIGHT, GRID_HEIGHT),
-        zA * GRID_DEPTH,
+        project(xA, 0, 1, -GRID.width / 2, GRID.width / 2),
+        project(yA, 0, 1, -GRID.height / 2, GRID.height / 2),
+        (zA * GRID.depth) / 2,
       )
       const pointB = $v(
-        project(xB, 0, 1, -GRID_WIDTH, GRID_WIDTH),
-        project(yB, 0, 1, -GRID_HEIGHT, GRID_HEIGHT),
-        zB * GRID_DEPTH,
+        project(xB, 0, 1, -GRID.width / 2, GRID.width / 2),
+        project(yB, 0, 1, -GRID.height / 2, GRID.height / 2),
+        (zB * GRID.depth) / 2,
       )
       const pointC = $v(
-        project(xC, 0, 1, -GRID_WIDTH, GRID_WIDTH),
-        project(yC, 0, 1, -GRID_HEIGHT, GRID_HEIGHT),
-        zC * GRID_DEPTH,
+        project(xC, 0, 1, -GRID.width / 2, GRID.width / 2),
+        project(yC, 0, 1, -GRID.height / 2, GRID.height / 2),
+        (zC * GRID.depth) / 2,
       )
       const pointD = $v(
-        project(xD, 0, 1, -GRID_WIDTH, GRID_WIDTH),
-        project(yD, 0, 1, -GRID_HEIGHT, GRID_HEIGHT),
-        zD * GRID_DEPTH,
+        project(xD, 0, 1, -GRID.width / 2, GRID.width / 2),
+        project(yD, 0, 1, -GRID.height / 2, GRID.height / 2),
+        (zD * GRID.depth) / 2,
       )
 
       quadrilateral2d(pointA, pointB, pointC, pointD, {
