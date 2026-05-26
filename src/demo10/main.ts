d6470c3a5af1c7d94b5a092496997c5de68c62e0
Cassiano D'Andrea <cassiano.dandrea@gmail.com>
2026-05-26 12:39:39 -0300

    Refatorando o demo 10

--- src/demo10/main.ts
+++ src/demo10/main.ts
@@ -20,16 +20,20 @@ import { rotateZ, quadrilateral2d } from '../primitives.ts'
 
 // -------------------------------------------------------------------------------------------------
 
-const NOISE_DIMENSIONS = [30, 30] as [number, number]
+const NOISE_DIMENSIONS = [50, 50] as [number, number]
+
+const GRID_WIDTH = 250
+const GRID_HEIGHT = 250
+const GRID_DEPTH = 200
 
 const noise = new PerlinNoise({
   // seed: 123,
-  gridSize: NOISE_DIMENSIONS as [number, number], // 2d grid
+  gridSize: NOISE_DIMENSIONS, // 2d grid
 })
 
 const noiseValues = timesMapN(NOISE_DIMENSIONS, (i, j) => {
-  const x = i / NOISE_DIMENSIONS[0]
-  const y = j / NOISE_DIMENSIONS[1]
+  const x = i / NOISE_DIMENSIONS[0] // 0 ≤ x ≤ 1
+  const y = j / NOISE_DIMENSIONS[1] // 0 ≤ y ≤ 1
 
   const k = noise.noise([x, y])
 
@@ -47,26 +51,40 @@ const draw = () => {
 
   render3dAxes()
 
-  const width = 200
-  const height = 200
-  const depth = 300
+  for (let i = 0; i < NOISE_DIMENSIONS[0] - 1; i++) {
+    for (let j = 0; j < NOISE_DIMENSIONS[1] - 1; j++) {
+      const { x: xA, y: yA, z: zA } = noiseValues[i][j]
+      const { x: xB, y: yB, z: zB } = noiseValues[i + 1][j]
+      const { x: xC, y: yC, z: zC } = noiseValues[i + 1][j + 1]
+      const { x: xD, y: yD, z: zD } = noiseValues[i][j + 1]
 
-  timesMapN([NOISE_DIMENSIONS[0] - 1, NOISE_DIMENSIONS[1] - 1], (i, j) => {
-    const { x: xA, y: yA, z: zA } = noiseValues[i][j]
-    const { x: xB, y: yB, z: zB } = noiseValues[i + 1][j]
-    const { x: xC, y: yC, z: zC } = noiseValues[i + 1][j + 1]
-    const { x: xD, y: yD, z: zD } = noiseValues[i][j + 1]
+      const pointA = $v(
+        (xA * 2 - 1) * GRID_WIDTH,
+        (yA * 2 - 1) * GRID_HEIGHT,
+        zA * GRID_DEPTH,
+      )
+      const pointB = $v(
+        (xB * 2 - 1) * GRID_WIDTH,
+        (yB * 2 - 1) * GRID_HEIGHT,
+        zB * GRID_DEPTH,
+      )
+      const pointC = $v(
+        (xC * 2 - 1) * GRID_WIDTH,
+        (yC * 2 - 1) * GRID_HEIGHT,
+        zC * GRID_DEPTH,
+      )
+      const pointD = $v(
+        (xD * 2 - 1) * GRID_WIDTH,
+        (yD * 2 - 1) * GRID_HEIGHT,
+        zD * GRID_DEPTH,
+      )
 
-    const pointA = $v((xA * 2 - 1) * width, (yA * 2 - 1) * height, zA * depth)
-    const pointB = $v((xB * 2 - 1) * width, (yB * 2 - 1) * height, zB * depth)
-    const pointC = $v((xC * 2 - 1) * width, (yC * 2 - 1) * height, zC * depth)
-    const pointD = $v((xD * 2 - 1) * width, (yD * 2 - 1) * height, zD * depth)
-
-    quadrilateral2d(pointA, pointB, pointC, pointD, {
-      isDoubleSided: true,
-      color: 'white',
-    })
-  })
+      quadrilateral2d(pointA, pointB, pointC, pointD, {
+        isDoubleSided: true,
+        color: 'white',
+      })
+    }
+  }
 }
 
 const onPaused = () => {
