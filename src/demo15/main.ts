3ff2b224e9830aebd9afce945f2edfd386d2df3f
Cassiano D'Andrea <cassiano.dandrea@gmail.com>
2026-07-02 12:25:06 -0300

    Revendo demo 15

--- src/demo15/main.ts
+++ src/demo15/main.ts
@@ -6,11 +6,10 @@ import {
 } from '../constants.ts'
 import {
   createFrameLoop,
-  millis,
   frameCount,
   fps,
-  logJson,
   timesForEach,
+  millis,
 } from '../utils.ts'
 import {
   background,
@@ -18,14 +17,14 @@ import {
   render3dAxes,
   resetTransformationMatrix,
   text2d,
-  rotateY,
   rotateX,
 } from '../primitives.ts'
 import { $v, Vector3d } from '../vector_3d.ts'
-import { PI, floor, map } from '../math_utils.ts'
+import { PI, floor, map, max, min } from '../math_utils.ts'
 import { ElbowShapeOptions, elbow } from '../elbow_primitives.ts'
-import { sample } from '../utils.ts'
+import { sample, logJson } from '../utils.ts'
 import { Tuple } from '../utility_types.ts'
+import { translate, rotateY, scale } from '../primitives.ts'
 
 // -------------------------------------------------------------------------------------------------
 
@@ -33,8 +32,10 @@ const CIRCLE_SEGMENTS = 16
 const OPACITY = 0.5
 const RADIUS = 100
 const CIRCLE_SLICES = 16
-const TOTAL_ELBOWS = 30
+const TOTAL_ELBOWS = 100
 const COLOR = 'peachpuff'
+
+const MAX_RETRY_COUNT = 100
 
 const options: ElbowShapeOptions = {
   circleSegments: CIRCLE_SEGMENTS,
@@ -79,7 +80,7 @@ timesForEach(TOTAL_ELBOWS, () => {
   let invalidVisit = true
   let retryCount = 0
 
-  while (invalidVisit && retryCount <= 4) {
+  while (invalidVisit && retryCount <= MAX_RETRY_COUNT) {
     nextAxis = sample(AXES_TRANSITIONS[previousAxis]) as AxesNamesType
     retryCount++
 
@@ -91,7 +92,7 @@ timesForEach(TOTAL_ELBOWS, () => {
       ) !== -1
   }
 
-  stopGeneration = retryCount > 4
+  stopGeneration = retryCount > MAX_RETRY_COUNT
 
   const savedPreviousAxis = previousAxis
 
@@ -102,6 +103,28 @@ timesForEach(TOTAL_ELBOWS, () => {
 
 logJson({ breadcrumbs })
 
+const visitedCoords = breadcrumbs.reduce(
+  (acc, item) => {
+    acc[0].push(item.x)
+    acc[1].push(item.y)
+    acc[2].push(item.z)
+
+    return acc
+  },
+  [[], [], []] as Tuple<number[], 3>,
+)
+
+const averageCoords = $v(
+  min(...visitedCoords[0]) +
+    (max(...visitedCoords[0]) - min(...visitedCoords[0])) / 2,
+  min(...visitedCoords[1]) +
+    (max(...visitedCoords[1]) - min(...visitedCoords[1])) / 2,
+  min(...visitedCoords[2]) +
+    (max(...visitedCoords[2]) - min(...visitedCoords[2])) / 2,
+)
+
+const centerCoords = averageCoords.mult(-RADIUS)
+
 // -------------------------------------------------------------------------------------------------
 
 const draw = () => {
@@ -110,10 +133,14 @@ const draw = () => {
 
   background('lightGray')
 
+  scale(1 / 2)
+
   rotateX(PI / 4)
   rotateY(-millis() / 2000)
 
   render3dAxes()
+
+  translate(centerCoords)
 
   elbowSequence.forEach(([source, destination], i) => {
     const hue = floor(map(i, 0, TOTAL_ELBOWS - 1, 0, 360))
