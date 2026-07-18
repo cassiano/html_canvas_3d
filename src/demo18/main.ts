7de310810fb807b23c235e013e34e59af8e3b3ff
Cassiano D'Andrea <cassiano.dandrea@gmail.com>
2026-07-18 12:17:09 -0300

    Substituindo o demo 18

--- src/demo18/main.ts
+++ src/demo18/main.ts
@@ -1,4 +1,4 @@
-import { FPS, FPS_LOGGING_FRAME_PERIOD } from '../constants.ts'
+import { FPS, FPS_LOGGING_FRAME_PERIOD, ORIGIN } from '../constants.ts'
 import { createFrameLoop, frameCount, fps, millis } from '../utils.ts'
 import {
   background,
@@ -6,20 +6,21 @@ import {
   render3dAxes,
   resetTransformationMatrix,
   text2d,
-  rotateY,
+  cube,
+  translate,
   rotateX,
-  line,
+  rotateY,
+  point,
 } from '../primitives.ts'
 import { $v } from '../vector_3d.ts'
-import { PI, sin, TWO_PI } from '../math_utils.ts'
-import { animation, isolateTransformations } from '../primitives.ts'
+import { elbow } from '../elbow_primitives.ts'
+import { PI } from '../math_utils.ts'
 
 // -------------------------------------------------------------------------------------------------
 
-const X_STEP = 0.1
-const SCALE = 20
+const RADIUS = 200
 
-const getPoint = (x: number) => $v(x, sin(x), 0)
+// -------------------------------------------------------------------------------------------------
 
 const draw = () => {
   // console.log({ fps: fps(), millis: millis(), frameCount: frameCount() })
@@ -32,18 +33,12 @@ const draw = () => {
 
   render3dAxes()
 
-  for (let angle = 0; angle < TWO_PI; angle += TWO_PI / 72) {
-    isolateTransformations(() => {
-      rotateY(angle)
+  elbow.y.x(RADIUS, { isDoubleSided: true })
 
-      for (let x = 0; x < animation.width / 2 / SCALE; x += X_STEP) {
-        const pointA = getPoint(x).mult(SCALE)
-        const pointB = getPoint(x + X_STEP).mult(SCALE)
+  translate(0, RADIUS / 2, 0)
+  cube(RADIUS, { opacity: 0.2 })
 
-        line(pointA, pointB, { noSplit: true })
-      }
-    })
-  }
+  point(ORIGIN, { size: 10, color: 'red' })
 }
 
 const onPaused = () => {
