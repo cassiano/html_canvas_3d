8eec36e6b535605a6641f792f1fab8d57189f847
Cassiano D'Andrea <cassiano.dandrea@gmail.com>
2026-07-19 00:12:28 -0300

    Incluindo formulário no demo 18

--- src/demo18/main.ts
+++ src/demo18/main.ts
@@ -1,5 +1,12 @@
 import { FPS, FPS_LOGGING_FRAME_PERIOD, ORIGIN } from '../constants.ts'
-import { createFrameLoop, frameCount, fps, millis } from '../utils.ts'
+import {
+  createFrameLoop,
+  frameCount,
+  fps,
+  millis,
+  createToggle,
+  createDemoControlPanel,
+} from '../utils.ts'
 import {
   background,
   render3dScene,
@@ -22,6 +29,33 @@ const RADIUS = 200
 
 // -------------------------------------------------------------------------------------------------
 
+// Get the canvas container
+const canvasContainer = document.getElementById('canvas-container')
+if (!canvasContainer) throw new Error('canvasContainer not found')
+
+let demoControlPanel: HTMLDivElement | null
+
+type FormType = {
+  toggles?: Record<'rotateAroundYAxis', ReturnType<typeof createToggle>>
+}
+
+export const demoForm: FormType = {}
+
+const createDemoControls = () => {
+  demoControlPanel = createDemoControlPanel(canvasContainer)
+
+  demoForm.toggles = {
+    rotateAroundYAxis: createToggle({
+      label: 'Rotate around Y-axis?',
+      value: true,
+      showValue: false,
+      container: demoControlPanel,
+    }),
+  }
+}
+
+// -------------------------------------------------------------------------------------------------
+
 const draw = () => {
   // console.log({ fps: fps(), millis: millis(), frameCount: frameCount() })
   if (frameCount() % FPS_LOGGING_FRAME_PERIOD === 0) console.log({ fps: fps() })
@@ -29,7 +63,9 @@ const draw = () => {
   background('lightGray')
 
   rotateX(PI / 4)
-  rotateY(-millis() / 2000)
+
+  if (demoForm.toggles?.rotateAroundYAxis.getValue()) rotateY(-millis() / 2000)
+  else rotateY(PI / 6)
 
   render3dAxes()
 
@@ -45,7 +81,7 @@ const onPaused = () => {
   text2d('PAUSED', $v(0, 300))
 }
 
-const { start, stop } = createFrameLoop(
+const { start: startFrameLoop, stop: stopFrameLoop } = createFrameLoop(
   () => {
     resetTransformationMatrix()
     draw()
@@ -54,5 +90,17 @@ const { start, stop } = createFrameLoop(
   onPaused,
   FPS,
 )
+
+const start = () => {
+  createDemoControls()
+  startFrameLoop()
+}
+
+const stop = () => {
+  demoControlPanel?.remove()
+  demoControlPanel = null
+
+  stopFrameLoop()
+}
 
 export { start, stop }
