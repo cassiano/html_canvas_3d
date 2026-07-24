600b98dfb14a912b80942a9e226c3f3d7b7a7a68
Cassiano D'Andrea <cassiano.dandrea@gmail.com>
2026-07-24 11:17:53 -0300

    Refatorando demo 20

--- src/demo20/main.ts
+++ src/demo20/main.ts
@@ -9,62 +9,117 @@ import {
 } from '../primitives.ts'
 import { $v } from '../vector_3d.ts'
 import { PI } from '../math_utils.ts'
-import { saturnRing } from '../primitives.ts'
+import { saturnRing, rotateZ, rotateY } from '../primitives.ts'
 import { isolateTransformations } from '../primitives.ts'
-import {
-  autoRotationEnabled,
-  sphere,
-  rotateY,
-  rotateX,
-  rotateZ,
-} from '../primitives.ts'
+import { autoRotationEnabled, sphere, rotateX } from '../primitives.ts'
 
 // -------------------------------------------------------------------------------------------------
 
-const RADIUS = 100
+type SolarSystemBody = {
+  radius: number
+  color: string
+}
+
+const RADIUS_RATIO = 1 / 380
+
+const SATURN_DATA: SolarSystemBody = {
+  radius: 58232, // Mean radius in km.
+  color: 'orange',
+}
+
+const SATURN_RINGS_DATA: Record<string, { width: number; color: string }> = {
+  // Width in km.
+  a: { width: 14585, color: 'lightPink' },
+  b: { width: 25554, color: 'lightGreen' },
+  c: { width: 17357, color: 'purple' },
+  d: { width: 7594, color: 'black' },
+}
+
+// -------------------------------------------------------------------------------------------------
+
+export const renderSaturn = (planet: SolarSystemBody, radiusRatio: number) => {
+  const { radius, color } = planet
+
+  isolateTransformations(() => {
+    rotateX(PI / 2)
+
+    sphere(radius * radiusRatio, { color })
+  })
+
+  // [/doc_img/main.ts/2026-07-23-22-58-31.png]
+  let startingRadius = radius * radiusRatio
+
+  isolateTransformations(() => {
+    rotateZ(millis() / 1500)
+
+    // D ring.
+    saturnRing(
+      startingRadius,
+      startingRadius + SATURN_RINGS_DATA.d.width * radiusRatio,
+      64,
+      {
+        color: SATURN_RINGS_DATA.d.color,
+        opacity: 0.5,
+      },
+    )
+
+    startingRadius += SATURN_RINGS_DATA.d.width * radiusRatio
+
+    // C ring.
+    saturnRing(
+      startingRadius,
+      startingRadius + SATURN_RINGS_DATA.c.width * radiusRatio,
+      64,
+      {
+        color: SATURN_RINGS_DATA.c.color,
+        opacity: 0.5,
+      },
+    )
+
+    startingRadius += SATURN_RINGS_DATA.c.width * radiusRatio
+
+    // B ring.
+    saturnRing(
+      startingRadius,
+      startingRadius + SATURN_RINGS_DATA.b.width * radiusRatio,
+      64,
+      {
+        color: SATURN_RINGS_DATA.b.color,
+        opacity: 0.5,
+      },
+    )
+
+    startingRadius += SATURN_RINGS_DATA.b.width * radiusRatio
+
+    // A ring.
+    saturnRing(
+      startingRadius,
+      startingRadius + SATURN_RINGS_DATA.a.width * radiusRatio,
+      64,
+      {
+        color: SATURN_RINGS_DATA.a.color,
+        opacity: 0.5,
+      },
+    )
+  })
+}
 
 // -------------------------------------------------------------------------------------------------
 
 const draw = () => {
-  // console.log({ fps: fps(), millis: millis(), frameCount: frameCount() })
   if (frameCount() % FPS_LOGGING_FRAME_PERIOD === 0) console.log({ fps: fps() })
 
   background('black')
 
-  rotateX(PI / 20)
-  rotateZ(-PI / 3.5)
+  rotateY(PI / 2.2)
+  rotateX(-PI / 3.5)
 
-  if (autoRotationEnabled) rotateY(-millis() / 2000)
-  else rotateY(PI / 6)
+  if (autoRotationEnabled) rotateZ(-millis() / 5000)
+  else rotateZ(PI / 6)
 
   render3dAxes()
 
-  sphere(RADIUS, { color: 'lightSalmon' })
-
-  isolateTransformations(() => {
-    rotateY(millis() / 1000)
-    rotateX(PI / 2)
-
-    saturnRing(RADIUS + 20, RADIUS + 40, 64, {
-      color: 'purple',
-      opacity: 0.5,
-    })
-
-    saturnRing(RADIUS + 40, RADIUS + 80, 64, {
-      color: 'lightGreen',
-      opacity: 0.5,
-    })
-
-    saturnRing(RADIUS + 80, RADIUS + 85, 64, {
-      color: 'turquoise',
-      opacity: 0.5,
-    })
-
-    saturnRing(RADIUS + 85, RADIUS + 105, 64, {
-      color: 'lightPink',
-      opacity: 0.5,
-    })
-  })
+  renderSaturn(SATURN_DATA, RADIUS_RATIO)
 }
 
 const onPaused = () => {
